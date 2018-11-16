fd1ab16fe9cf68a409a0c6f28b04065eee70433e
Yuriy <ts.yuriy.84@gmail.com>
2018-11-17 01:18:06 +0300

    вернул в нормальный вид

--- script.js
+++ script.js
@@ -16,10 +16,32 @@ let appData = {
     savings: false
 };
 
-appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = +prompt("Во сколько обойдется?", "");
-appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = +prompt("Во сколько обойдется?", "");
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
 
+    if ((typeof (a) === 'string') && (typeof (a) != null) && (typeof (a) != '')
+    && (typeof (b) != null) && (typeof (b) != '') && a.lengrh < 50 && b.length < 50) {
+        appData.expenses[a] = b;
+    }else{
+        --i;
+        if(i<0){
+            i = 0;
+        }
+    }
+};
 
+appData.moneyPerDay = appData.budget / MONTH;
 
-alert(appData.budget/MONTH);
+// let i = 0;
+// while(i<2){
+//     appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = +prompt("Во сколько обойдется?", "");
+//     i++;
+// }
+
+// let i = 0;
+
+// do{
+// appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = +prompt("Во сколько обойдется?", "");
+// i++;} while(i<2);
 
