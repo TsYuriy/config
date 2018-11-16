71825687a651cef93c0d3e38905c6e9ffa24e770
Yuriy <ts.yuriy.84@gmail.com>
2018-11-17 01:13:59 +0300

    изменения

--- lesson-2/script.js
+++ lesson-2/script.js
@@ -16,20 +16,31 @@ let appData = {
     savings: false
 };
 
-for(let i = 0; i < 2; i++){
-    appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = +prompt("Во сколько обойдется?", "");
-}
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if ((typeof (a) === 'string') && (typeof (a) != null) && (typeof (a) != '')
+    && (typeof (b) != null) && (typeof (b) != '') && a.lengrh < 50 && b.length < 50) {
+        appData.expenses[a] = b;
+    }else{
+        --i;
+        if(i<0){
+            i = 0;
+        }
+    }
+};
+
+appData.moneyPerDay = appData.budget / MONTH;
 
 // let i = 0;
 // while(i<2){
 //     appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = +prompt("Во сколько обойдется?", "");
 //     i++;
 // }
-let i = 0;
+
+// let i = 0;
 
 // do{
 // appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = +prompt("Во сколько обойдется?", "");
 // i++;} while(i<2);
-
-console.log(appData.budget/MONTH);
-
