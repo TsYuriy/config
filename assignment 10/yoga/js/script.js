1008b635cc37b9e82970cb357905d9afaa8f81d1
Yuriy <ts.yuriy.84@gmail.com>
2019-01-16 23:45:20 +0300

    написан функционал таймера обратного отсчета до указанной даты

--- assignment 10/yoga/js/script.js
+++ assignment 10/yoga/js/script.js
@@ -38,7 +38,47 @@ window.addEventListener('DOMContentLoaded', function(){  // как только 
         }
     });
 
+    let deadLine = '2019-01-18';
 
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000)%60),
+            minutes = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/1000/60/60));
+
+            if(t <= 0) {
+                seconds = minutes = hours = '00';
+            }
+
+            return {
+                'total': t,
+                'seconds': seconds,
+                'minutes': minutes,
+                'hours': hours
+            };
+    }
+
+    function setClock(id, endTime){
+        let t = document.getElementById(id),
+            hours = document.querySelector('.hours'),
+            minutes = document.querySelector('.minutes'),
+            seconds = document.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock() {
+                let t = getTimeRemaining(endTime);
+                hours.textContent = (String(t.hours).length < 2) ? '0' + t.hours : t.hours;
+                minutes.textContent = (String(t.minutes).length < 2) ? '0' + t.minutes : t.minutes;
+                seconds.textContent = (String(t.seconds).length < 2) ? '0' + t.seconds : t.seconds;
+                
+                if(total <= 0) {
+                    clearInterval(timeInterval);
+                }
+
+            }
+    }
+
+    setClock('timer', deadLine);
 
 
 });
